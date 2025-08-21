8edde8742fd47e132fbc292edd52b02737e8b449
Aditya Thakur <adityathakur19200@gmail.com>
2025-08-21 14:34:06 +0530

    uyo78

--- components/bills/create-bill-dialog.tsx
+++ components/bills/create-bill-dialog.tsx
@@ -870,10 +870,10 @@ export default function CreateBillDialog({ open, onOpenChange, packages, onSucce
               {loading ? (
                 <>
                   <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                  Creating Entry...
+                  Creating Bill...
                 </>
               ) : (
-                "Add to Daily Sheet"
+                "Process Bill"
               )}
             </Button>
           </div>
